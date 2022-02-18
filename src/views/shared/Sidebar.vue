96ccc433120a64b36ca9944f787f12fc1ea1d15a
laurion <klauriondareich@gmail.com>
2022-02-18 11:56:04 +0100

    sidebar elements font size inscreased, statistiques and paiements menu masked

--- src/views/shared/Sidebar.vue
+++ src/views/shared/Sidebar.vue
@@ -9,8 +9,8 @@
                     <li><router-link to="/salon" title=""><i class="fa fa-building"></i><span>Mon salon</span></router-link></li>
                     <li><router-link to="/appointments" title=""><i class="fa fa-calendar"></i><span>Tous les rendez-vous</span></router-link></li>
                     <li><router-link to="/allcustomers" title=""><i class="fa fa-users"></i><span>Tous les clients</span></router-link></li>
-                    <li><router-link to="/statistics" title=""><i class="fa fa-bar-chart"></i><span>Statistiques</span></router-link></li>
-                    <li><router-link to="/payments" title=""><i class="fa fa-credit-card"></i><span>Paiements</span></router-link></li>
+                    <!-- <li><router-link to="/statistics" title=""><i class="fa fa-bar-chart"></i><span>Statistiques</span></router-link></li> -->
+                    <!-- <li><router-link to="/payments" title=""><i class="fa fa-credit-card"></i><span>Paiements</span></router-link></li> -->
                     <li><router-link to="/comments" title=""><i class="fa fa-comment"></i><span>Notes et commentaires</span></router-link></li>
                     <li><router-link to="/gallery" title=""><i class="fa fa-photo"></i><span>Galerie</span></router-link></li>
                 </ul>
