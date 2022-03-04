c97cff9ad8fe3e71a4b9c95bdba3b09a6d4731dc
laurion <klauriondareich@gmail.com>
2022-03-04 16:08:02 +0100

    profile menu added

--- src/views/shared/Sidebar.vue
+++ src/views/shared/Sidebar.vue
@@ -2,17 +2,18 @@
   <header>
     <div class="side-menus">
         <div class="side-header">
-            <div class="logo"><router-link title="" to="/home"><img alt="" src="images/b-logo.png"></router-link></div>
+            <div class="logo"><router-link to="/home"><img alt="" src="images/b-logo.png"></router-link></div>
             <nav class="slide-menu">
                 <ul class="parent-menu">                                           
-                    <li><router-link to="/home" title=""><i class="fa fa-home"></i><span>Accueil</span></router-link></li>
-                    <li><router-link to="/salon" title=""><i class="fa fa-building"></i><span>Mon salon</span></router-link></li>
-                    <li><router-link to="/appointments" title=""><i class="fa fa-calendar"></i><span>Tous les rendez-vous</span></router-link></li>
-                    <li><router-link to="/allcustomers" title=""><i class="fa fa-users"></i><span>Tous les clients</span></router-link></li>
-                    <!-- <li><router-link to="/statistics" title=""><i class="fa fa-bar-chart"></i><span>Statistiques</span></router-link></li> -->
-                    <!-- <li><router-link to="/payments" title=""><i class="fa fa-credit-card"></i><span>Paiements</span></router-link></li> -->
-                    <li><router-link to="/comments" title=""><i class="fa fa-comment"></i><span>Notes et commentaires</span></router-link></li>
-                    <li><router-link to="/gallery" title=""><i class="fa fa-photo"></i><span>Galerie</span></router-link></li>
+                    <li><router-link to="/home"><i class="fa fa-home"></i><span>Accueil</span></router-link></li>
+                    <li><router-link to="/salon"><i class="fa fa-building"></i><span>Mon salon</span></router-link></li>
+                    <li><router-link to="/appointments"><i class="fa fa-calendar"></i><span>Tous les rendez-vous</span></router-link></li>
+                    <li><router-link to="/allcustomers"><i class="fa fa-users"></i><span>Tous les clients</span></router-link></li>
+                    <!-- <li><router-link to="/statistics"><i class="fa fa-bar-chart"></i><span>Statistiques</span></router-link></li> -->
+                    <!-- <li><router-link to="/payments"><i class="fa fa-credit-card"></i><span>Paiements</span></router-link></li> -->
+                    <li><router-link to="/comments"><i class="fa fa-comment"></i><span>Notes et commentaires</span></router-link></li>
+                    <li><router-link to="/gallery"><i class="fa fa-photo"></i><span>Galerie</span></router-link></li>
+                    <li><router-link to="/user_profile"><i class="fa fa-user"></i><span>Mon profil</span></router-link></li>
                 </ul>
             </nav>
         </div>
