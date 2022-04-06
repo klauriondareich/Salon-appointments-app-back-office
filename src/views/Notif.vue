b42e6c1631dbdab444d80da38e65242592e23c07
laurion <klauriondareich@gmail.com>
2022-04-06 10:23:40 +0200

    Notif.vue component added

---
+++ src/views/Notif.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="main-content">
+        <div class="panel-body">
+            <div class="col-lg-6 col-sm-6">
+                <div class="widget">
+                    <div class="widget-title no-margin">
+                        <h4>news feed</h4>
+                        <span>stories posted today</span>
+                        <ul class="widget-controls">
+                            <li title="Refresh" class="refresh-content"><i class="fa fa-refresh"></i></li>
+                            <li title="Maximize" class="expand-content"><i class="icon-frame"></i></li>
+                            <li title="More Options" class="more-option"><i class="ti-more-alt"></i></li>
+                        </ul>
+                    </div>
+                    <div class="news-feed-list">
+                        <div class="new-box"> <img src="/images/resources/news1.jpg" alt="">
+                            <div class="news-detail">
+                                <h2><a href="#" title="">Trevor Hansen</a></h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipis auris justo interdum vitae aliquet congue...</p>
+                            </div>
+                        </div>
+                        <div class="new-box"> <img src="/images/resources/news2.jpg" alt="">
+                            <div class="news-detail">
+                                <h2><a href="#" title="">Johan Doe</a></h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipis auris justo interdum vitae aliquet congue...</p>
+                            </div>
+                        </div>
+                        <div class="new-box"> <img src="/images/resources/news3.jpg" alt="">
+                            <div class="news-detail">
+                                <h2><a href="#" title="">Sandra Adams</a></h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipis auris justo interdum vitae aliquet congue...</p>
+                            </div>
+                        </div>
+                        <div class="new-box"> <img src="/images/resources/news1.jpg" alt="">
+                            <div class="news-detail">
+                                <h2><a href="#" title="">Trevor Hansen</a></h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipis auris justo interdum vitae aliquet congue...</p>
+                            </div>
+                        </div>
+                        <div class="new-box"> <img src="/images/resources/news3.jpg" alt="">
+                            <div class="news-detail">
+                                <h2><a href="#" title="">Sandra Adams</a></h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipis auris justo interdum vitae aliquet congue...</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
