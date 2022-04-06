270bf24e89e26f4471bb161db8596d1c1455c934
laurion <klauriondareich@gmail.com>
2022-04-06 11:46:29 +0200

    getting cancel appoints from firestore and displaying in the view

--- src/views/Notif.vue
+++ src/views/Notif.vue
@@ -1,46 +1,19 @@
 <template>
     <div class="main-content">
         <div class="panel-body">
-            <div class="col-lg-6 col-sm-6">
+            <div class="col-lg-12 col-sm-12">
                 <div class="widget">
                     <div class="widget-title no-margin">
-                        <h4>news feed</h4>
-                        <span>stories posted today</span>
-                        <ul class="widget-controls">
-                            <li title="Refresh" class="refresh-content"><i class="fa fa-refresh"></i></li>
-                            <li title="Maximize" class="expand-content"><i class="icon-frame"></i></li>
-                            <li title="More Options" class="more-option"><i class="ti-more-alt"></i></li>
-                        </ul>
+                        <h4>Toutes les notificaitoions</h4>
                     </div>
                     <div class="news-feed-list">
-                        <div class="new-box"> <img src="/images/resources/news1.jpg" alt="">
+                        <div class="new-box" v-for="(item, index) in cancel_appointments" :key="index">
                             <div class="news-detail">
-                                <h2><a href="#" title="">Trevor Hansen</a></h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipis auris justo interdum vitae aliquet congue...</p>
-                            </div>
-                        </div>
-                        <div class="new-box"> <img src="/images/resources/news2.jpg" alt="">
-                            <div class="news-detail">
-                                <h2><a href="#" title="">Johan Doe</a></h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipis auris justo interdum vitae aliquet congue...</p>
-                            </div>
-                        </div>
-                        <div class="new-box"> <img src="/images/resources/news3.jpg" alt="">
-                            <div class="news-detail">
-                                <h2><a href="#" title="">Sandra Adams</a></h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipis auris justo interdum vitae aliquet congue...</p>
-                            </div>
-                        </div>
-                        <div class="new-box"> <img src="/images/resources/news1.jpg" alt="">
-                            <div class="news-detail">
-                                <h2><a href="#" title="">Trevor Hansen</a></h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipis auris justo interdum vitae aliquet congue...</p>
-                            </div>
-                        </div>
-                        <div class="new-box"> <img src="/images/resources/news3.jpg" alt="">
-                            <div class="news-detail">
-                                <h2><a href="#" title="">Sandra Adams</a></h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipis auris justo interdum vitae aliquet congue...</p>
+                                <p>Votre client <span class="font-weight-bold">{{item.customer_name}}</span> a annulé son rdv</p>
+                                <p>Pour la prestation de <span class="font-weight-bold">{{item.work_name.split("\n").join(" / ")}}</span></p>
+                                <p>D'un montant <span class="font-weight-bold">{{item.total}} FCFA</span></p>
+                                <p>Date de rdv : <span class="font-weight-bold">{{item.stamp | formatDate}}</span></p>
+                                <span v-if="item.status =='cancelled_by_user'" class="priority high ">Annulé</span>
                             </div>
                         </div>
                     </div>
@@ -49,3 +22,43 @@
         </div>
     </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import firebase from '../firebase/init'
+// import Loader from './shared/Loader.vue'
+// import MobileHeader from '../components/MobileHeader.vue'
+
+export default {
+  name: 'Notif',
+//   components: {Loader, MobileHeader},
+
+  data(){
+    
+    return{
+       cancel_appointments: [],
+       appointCancelRef: firebase.firestore().collection("appointment"), 
+
+    }
+  },
+
+  created(){
+      console.log("initi")
+      this.appointCancelRef.where("status", "==", "cancelled_by_user").orderBy("stamp", "desc").onSnapshot((snapshot) =>{
+      if(!snapshot.empty){
+            this.cancel_appointments = [];
+
+            snapshot.forEach((doc) =>{
+                let obj = doc.data();
+                obj.id = doc.id;
+
+                console.log("obj", obj)
+                this.cancel_appointments.push(obj)
+            })
+         }
+         else console.log("madesso")
+    })
+
+  }
+}
+</script>
