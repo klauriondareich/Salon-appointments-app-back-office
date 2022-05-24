6804c5f4cfdc4776e947d3531da2ec6ea692def7
laurion <klauriondareich@gmail.com>
2022-05-24 13:56:40 +0200

    MobileHeader redesigned

--- src/components/MobileHeader.vue
+++ src/components/MobileHeader.vue
@@ -1,39 +1,9 @@
 <template>
    <div class="responsive-header pt-5">
-            <div class="logo-area">
-                
-                <div class="user-head">
-                <div class="admin">
-                    <div class="admin-avatar" v-if="!isVisible" @click="isVisible = true">
-                        <img src="/images/user-icon.png" alt=""> <i class="online"></i> 
-                    </div>
-                    <div class="admin-avatar" v-if="isVisible" @click="isVisible = false">
-                        <img src="/images/user-icon.png" alt=""> <i class="online"></i> 
-                    </div>            
-                </div>
-                <div class="drop setting"  v-if="isVisible">                    
-                    <span class="drop-bottom"  @click="signOut()"><a href="#" title=""><i class="fa fa-sign-out"></i>Se déconnecter</a></span> </div>
-                </div>
-                <ul class="seting-area">
-
-                    <li class="text-white">{{username}}</li>
-                <!-- <li class="langages">
-                    <a title="" href="#">Eng</a>
-                    <ul class="drop language">
-                        <li class="lang-selected"><a href="#"><i class="fa fa-check"></i> Eng</a></li>
-                        <li><a href="#">Rus</a></li>
-                        <li><a href="#">Jap</a></li>
-                        <li><a href="#">Arb</a></li>
-                    </ul>
-                </li> -->
-                <li  @click="showSidebar()" v-if="!this.$store.state.sidebarState">
-                    <span class="icon-style"><i class="fa fa-bars"></i></span>
-                </li>
-                <li @click="hideSidebar()" v-if="this.$store.state.sidebarState">
-                    <span class="icon-style"><i class="fa fa-close"></i></span>
-                </li>
-            </ul>
-            </div>
+       
+       <p class="pt-5 pl-3">
+          <i class="fa fa-user"></i> Bienvenue {{username}}
+       </p>
             
         </div>
 </template>
@@ -48,20 +18,13 @@ export default {
 
     data(){
         return {
-            username: null,
+            username: null, 
             isVisible: false,
         }
     },
 
     methods:{
 
-        showSidebar(){
-          this.$store.state.sidebarState = true
-        },
-
-        hideSidebar(){  
-          this.$store.state.sidebarState = false
-        },
     },
 
     created(){
