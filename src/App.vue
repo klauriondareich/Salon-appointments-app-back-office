7c03c4baadc3be71b997aa9712d3dfe354fd374d
laurion <klauriondareich@gmail.com>
2022-05-25 11:41:50 +0200

    vue logo and commented scripts removed

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,9 @@
 </template>
 
 <script>
-// import  Topbar  from '../src/views/shared/Topbar.vue'
-// import  Sidebar from './views/shared/Sidebar.vue'
 
 export default {
-  // components:{Topbar, Sidebar}
+
 }
 </script>
 
