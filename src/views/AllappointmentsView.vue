1e9e14bf188195ed5eca2cb0bc1298f73c41fc2f
laurion <klauriondareich@gmail.com>
2022-08-05 11:14:10 +0100

    beep and background color changes when new appoint feature done

--- src/views/AllappointmentsView.vue
+++ src/views/AllappointmentsView.vue
@@ -390,6 +390,7 @@ export default {
                                     this.beepBg = true;
                                    
                                 }
+                                
                                 localStorage.setItem("firstLoad", "no");
                                 
                             
